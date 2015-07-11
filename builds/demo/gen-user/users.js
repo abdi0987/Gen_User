2badbc71cc1a9e14d6877ef39edf466a9f37c492
abdullahi0987 <abdullahimahamed0987@gmail>
2015-07-10 21:55:56 -0500

    Added Demo Dir

---
+++ builds/demo/gen-user/users.js
@@ -0,0 +1,28 @@
+angular.module('GenUser',[])
+
+.directive('genUser',['$http','$log',function($http,$log) {
+    return {
+        restrict: 'A',
+        scope: {
+            genUser: '@'
+        },
+        template: '{{text}}',
+        link: function (scope ,elem) {
+          $http.get('gen-user/userData.json').success(function (mdata) {
+            scope.data = mdata;
+            var value = scope.genUser;
+            var indexOfUser = Math.floor((Math.random() * scope.data.length) + 0);
+            console.log(scope.data[indexOfUser]);
+            if(value === 'name'){
+              scope.text = scope.data[indexOfUser].first_name + " " + scope.data[indexOfUser].last_name;
+            } else {
+              scope.text = scope.data[indexOfUser][value];
+            }
+          });
+        }
+    };
+}]);
+
+function UserGen(){
+    this.users = [];
+}
